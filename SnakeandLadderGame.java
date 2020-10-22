ae2e3804d8e32da0bead5f259fe720ce944e602a
Aishwarya <amuthyala511@gmail.com>
2020-10-22 08:19:47 +0530

    checked options for NoPlay,Ladder and Snake

--- SnakeandLadderGame.java
+++ SnakeandLadderGame.java
@@ -7,11 +7,31 @@ public class SnakeandLadderGame
 		System.out.println("This is a Snake and Ladder game simulation");
 		final int START_POSITION=0;
 		final int END_POSITION=100;
+		final int IS_NoPLAY=0;
+		final int IS_LADDER=1;
+		final int IS_SNAKE=2;
 		int position=0;
 		System.out.println("The player is at start position: "+START_POSITION);
 		Random random=new Random();
 		int dienumber=(random.nextInt(6)+1);
-		position=position+dienumber;
-		System.out.println("The player changed the position to: "+position);
+		System.out.println("dice is rolled and the die number is: "+dienumber);
+		int checkOption=(random.nextInt(3));
+		if(checkOption == IS_LADDER)
+		{
+			position=position+dienumber;
+         System.out.println("The player moves ahead by "+dienumber+" positions");
+			System.out.println("Current position of the player is: "+position);
+		}
+		else if(checkOption == IS_SNAKE)
+		{
+			position=position-dienumber;
+			System.out.println("The player moves behind by "+dienumber+" positions");
+			System.out.println("Current position of the player is: "+position);
+		}
+		else if(checkOption == IS_NoPLAY)
+		{
+			System.out.println("No play. The player stays in the same position");
+			System.out.println("Current position of the player is: "+position);
+		}
 	}
 }
