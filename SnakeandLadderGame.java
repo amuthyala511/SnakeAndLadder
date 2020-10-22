df757ccdf09a559e2ac96a9512197a05694df314
Aishwarya <amuthyala511@gmail.com>
2020-10-22 07:25:24 +0530

    Started the game with single player at start position

---
+++ SnakeandLadderGame.java
@@ -0,0 +1,9 @@
+public class SnakeandLadderGame
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is a Snake and Ladder game simulation");
+		int START_POSITION=0;
+		System.out.println("The player is at start position: "+START_POSITION);
+	}
+}
